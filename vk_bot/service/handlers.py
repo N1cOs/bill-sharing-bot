a5683cc3933f01435d2d3892a73e6fdbb53b4fa3
Nick <nickkarmatskikh@gmail.com>
2019-07-07 21:09:07 +0300

    change format of owe command

--- vk_bot/service/handlers.py
+++ vk_bot/service/handlers.py
@@ -2,41 +2,38 @@ import re
 
 import vk_bot.service.commands as cmd
 from vk_bot.exceptions import SyntaxException
+from .util import Util
 
 
 class OweHandler:
     PATTERN = r'^(?P<debtors>(?:\[.+?\|.+?\]\s*,?\s*)+){}\s(?P<user>\[.+?\|.+?\]\s)?' \
-              r'(?P<amount>\d+?[.,]?\d*?)\s(:?(?P<param>{}|{}|{})\s)?(?P<name>.+)$'
+              r'(?P<amount>\d+?[.,]?\d*?)\s(:?(?P<period>\d+?)\s)?(?P<name>.+)$'
 
     @staticmethod
     def match(text):
-        pattern = OweHandler.PATTERN.format(*(_('pattern.owe').split()))
+        pattern = OweHandler.PATTERN.format(_('cmd.owe'))
         return re.match(pattern, text)
 
     @staticmethod
-    def handle(match, data):
+    def handle(match, key):
         try:
             debtors = re.split(r',|(?<=\])\s+(?=\[)', match.group('debtors'))
             debtors_id = [Util.parse_user_id(d.strip()) for d in debtors]
 
             user = match.group('user')
-            lender_id = Util.parse_user_id(user) if user else data['from_id']
+            lender_id = Util.parse_user_id(user) if user else key.from_id
 
             amount_str = match.group('amount').replace(',', '.')
             amount = round(float(amount_str), 2)
-            regularity = match.group('param')
 
+            period = match.group('period')
+            if period is None:
+                period = 0
+            else:
+                period = int(period)
             name = match.group('name')
-            if regularity is None:
-                name = name.split(' ', 1)[1]
 
-            return cmd.handle_owe(lender_id, debtors_id, amount, regularity, name)
-        except Exception:
+            return cmd.handle_owe(key, lender_id, debtors_id, amount, period, name)
+        except Exception as e:
+            print(e)
             raise SyntaxException('Error occurred while parsing. Check format')
-
-
-class Util:
-    @staticmethod
-    def parse_user_id(text):
-        pipe_index = text.index('|')
-        return int(text[3:pipe_index])
