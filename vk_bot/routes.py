68f6d7735fefd897cf1b0350ebb78bad1bd13d53
Nick <nickkarmatskikh@gmail.com>
2019-07-20 16:57:37 +0500

    replace return string with sending message

--- vk_bot/routes.py
+++ vk_bot/routes.py
@@ -5,7 +5,7 @@ from flask import request, json, abort
 import vk_bot.service.handlers as hd
 import vk_bot.service.temp as t
 from vk_bot.exceptions import SyntaxException
-from vk_bot.service.util import Key
+from vk_bot.service.util import Key, Util
 from .app import app
 from .config import Config
 
@@ -24,19 +24,24 @@ def handle():
 
         key = Key(data['peer_id'], data['from_id'])
         temp = t.is_temp_state(key)
+
         try:
             if temp:
-                return t.handle(key, temp, text)
-
-            for Handler in HANDLERS:
-                match = Handler.match(text)
-                if match:
-                    handler = Handler(match, key)
-                    return handler.handle()
-
-            return 'error'
+                message = t.handle(key, temp, text)
+            else:
+                for Handler in HANDLERS:
+                    match = Handler.match(text)
+                    if match:
+                        handler = Handler(match, key)
+                        message = handler.handle()
+                        break
+                else:
+                    message = _('exception.cmd.not_recognised')
         except SyntaxException as e:
-            return e.message
+            message = e.message
+
+        Util.send_message(key.peer_id, message)
+        return 'ok'
     elif event_type == CONFIRMATION:
         return Config.VK_CONFIRMATION_STRING
     abort(400)
