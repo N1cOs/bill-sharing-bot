f6548faa01b3313fd534da2257dc6199e049b8e9
Nick <nickkarmatskikh@gmail.com>
2019-07-20 15:34:59 +0500

    fix vk token

--- vk_bot/app.py
+++ vk_bot/app.py
@@ -3,8 +3,8 @@ import gettext
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from vk_api import VkApi
 from redis import Redis
+from vk_api import VkApi
 
 from vk_bot.config import Config
 
@@ -19,4 +19,4 @@ redis = Redis(password=Config.REDIS_PASSWORD)
 ru = gettext.translation('msg', localedir=Config.LOCALE_DIR, languages=['ru'])
 ru.install()
 
-vk = VkApi(token=Config.VK_API_VERSION, api_version=Config.VK_API_VERSION)
+vk = VkApi(token=Config.VK_TOKEN, api_version=Config.VK_API_VERSION)
